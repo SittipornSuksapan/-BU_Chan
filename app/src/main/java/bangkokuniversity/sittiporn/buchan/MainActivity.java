9fa8a48046ee0632a0ef87d5b4dac422882d5602
Chan <chanzaloveza@gmail.com>
2015-11-14 16:02:36 +0700

    Finish Day1

--- app/src/main/java/bangkokuniversity/sittiporn/buchan/MainActivity.java
+++ app/src/main/java/bangkokuniversity/sittiporn/buchan/MainActivity.java
@@ -1,6 +1,8 @@
 package bangkokuniversity.sittiporn.buchan;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
                 buttonPayer.start();
 
                 //Intent to WebView
-                
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://androidthai.in.th/about-me.html"));
+                startActivity(objIntent);
 
             }  //event
         });
